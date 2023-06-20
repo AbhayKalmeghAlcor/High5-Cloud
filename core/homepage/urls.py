fc6339dc6f5bec194b0db1b3e1fba947ffeddb90
abhaykalmegh <abhayrameshwar.kalmegh@alcortech.com>
2023-06-20 17:25:55 +0530

    homepage added

--- core/homepage/urls.py
+++ core/homepage/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
-from . import views
+from django.urls import path
+from .views import Index
 
 urlpatterns = [
-
+    path('index/homepage/', Index.as_view(), name='index'),
 
 ]
