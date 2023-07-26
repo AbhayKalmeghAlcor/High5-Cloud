e829a4c1615ef6e6017e94d5f003f162a34f324b
satyamalcor <satyam.faujdar@alcortech.com>
2023-07-26 15:37:05 +0530

    Misc Fixes to staging

--- core/homepage/views.py
+++ core/homepage/views.py
@@ -222,9 +222,9 @@ class TransactionView(APIView, PaginationHandlerMixin):
 
     def post(self, request):
         sender_id = request.data.get('sender')
-        recipients_ids = request.data.get('recipients').split(",")
+        recipients_ids = request.data.getlist('recipients')
         point = int(request.data.get('point', 0))
-        hashtags_names = request.data.get('hashtags').split(",")
+        hashtags_names = request.data.getlist('hashtags')
         parent_id = request.data.get('parent_id', None)
         parent_transaction = None
 
